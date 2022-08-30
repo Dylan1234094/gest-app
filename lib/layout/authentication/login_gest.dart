94c421a2f12d49c764b8b3704a6ad93b6dbf7f52
Polarsh <dybsm98@outlook.es>
2022-08-30 01:39:20 -0500

    Updated login view

--- lib/layout/authentication/login_gest.dart
+++ lib/layout/authentication/login_gest.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:gest_app/layout/gest/guide/guide_detail.dart';
-import 'package:gest_app/layout/gest/tabs.dart';
 import 'package:gest_app/service/gestante_service.dart';
-import 'package:googleapis/fitness/v1.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 class LoginGestWidget extends StatelessWidget {
   const LoginGestWidget({Key? key}) : super(key: key);
@@ -25,25 +21,63 @@ class _LoginGestState extends State<LoginGest> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Iniciar Sesión"),
-      ),
+      appBar: AppBar(),
       body: SingleChildScrollView(
         child: Column(
           children: <Widget>[
             const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 6),
+              padding: EdgeInsets.only(top: 20),
               child: Text(
-                  "Inicie sesión con su cuenta de Google para continuar con la configuración de su cuenta"),
+                "Inicio de sesión",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+              ),
             ),
-            Align(
-              alignment: Alignment.center,
+            const Center(
               child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-                child: ElevatedButton(
-                  onPressed: () => {loginGestante(context)},
-                  child: const Text('INICIAR SESIÓN CON GOOGLE'),
+                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 25),
+                child: Text(
+                    "Inicie sesión con su cuenta de Google para continuar con la configuración de su cuenta"),
+              ),
+            ),
+            InkWell(
+              onTap: () {
+                loginGestante(context);
+              },
+              child: Ink(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black),
+                  color: Colors.white,
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.grey,
+                      blurRadius: 7,
+                      offset: Offset(0, 3), // changes position of shadow
+                    ),
+                  ],
                 ),
+                child: Padding(
+                  padding: const EdgeInsets.all(6),
+                  child: Wrap(
+                    crossAxisAlignment: WrapCrossAlignment.center,
+                    children: [
+                      Image.asset('Google_G_icon.png',
+                          width: 25,
+                          height: 25), // <-- Use 'Image.asset(...)' here
+                      const SizedBox(width: 4),
+                      const Text('Iniciar sesión con Google')
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 20),
+              width: 250,
+              height: 400,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('Gest_Login.png'), fit: BoxFit.fill),
+                shape: BoxShape.rectangle,
               ),
             ),
           ],
